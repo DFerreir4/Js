07d3f36c71f5bc8fe022a909360876dfb0c46ccb
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-06-15 21:36:10 -0300

    Condições parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitado. Multado!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'africa'
+console.log(`Vivendo em ${país}`)
+if(país != 'BR'){
+    console.log(` Você é Estrangeiro`)
+}else{
+    console.log(` Você é Brasileiro`)
+}
