4fbb4bfc5a45e84f9cac558d6584ae0a03147379
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-06-19 22:54:04 -0300

    atualização do projeto com imagens

---
+++ aula12ex/ex14/script.js
@@ -0,0 +1,36 @@
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 13
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >= 0 && hora < 12){
+    //bom dia
+    img.src = 'fonto-manha.png'
+}else if(hora >= 12 && hora < 18){
+    //boa tarde
+    img.src = 'fonto-tarde.png'
+    }
+    else{
+        //boa noite
+        img.src = './fonto-noite.png'
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
