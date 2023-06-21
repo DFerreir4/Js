a12e59e87125ed534aa4b49e8219c426bd7c233d
Diego Ferreira <115119146+DFerreir4@users.noreply.github.com>
2023-06-20 22:25:53 -0300

    incrementando mensagem no exercício.

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -2,23 +2,41 @@ function carregar(){
 
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
+var msg2 = window.document.getElementById('ola')
+var footer = window.document.getElementById('p-rodape')
 var data = new Date()
 //var hora = data.getHours()
-var hora = 19
+var hora = 6
 msg.innerHTML = `Agora são ${hora} horas.`
+
+
 
 if(hora >= 0 && hora < 12){
     //Bom dia!
     img.src = './foto-manha.png'
     document.body.style.background = '#e2cd9f'
+    msg2.innerHTML = 'Bom dia! Caro Usuário.'
+    msg2.style.fontWeight = 'bolder'
+    msg2.style.fontStyle = 'italic'
+    footer.style.color = '#ff9955'
+   
+    
 } else if (hora >= 12 && hora < 18){
     //Boa tarde!
     img.src = './foto-tarde.png'
     document.body.style.background = '#b9846f'
+    msg2.innerHTML = 'Boa tarde! Caro Usuário.'
+    msg2.style.fontWeight = 'bolder'
+    msg2.style.fontStyle = 'italic'
 }else{
     //Boa noite!
     img.src = './foto-noite.png'
     document.body.style.background = '#515154'
+    msg2.innerHTML = 'Boa noite! Caro Usuário.'
+    msg2.style.fontWeight = 'bolder'
+    msg2.style.fontStyle = 'italic'
 }
+
+
 
 }
